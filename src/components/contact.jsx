0e0d372af2adb2c31abb8b80f2cd2debd2b37427
vaibhavmahindru <500067728@stu.upes.ac.in>
2020-06-09 04:13:51 +0530

    CSS left

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -4,7 +4,63 @@ class contact extends Component {
   render() {
     return (
       <React.Fragment>
-        <h1>contact</h1>
+        <section className="contact">
+          <h4>
+            Drop me an email at{" "}
+            <a href="mailto:vaibhavmahindu04@gmail.com">
+              vaibhavmahindru04@gmail.com
+            </a>
+            <br />
+            <strong>OR </strong>
+            just simply fill the form beow and I will get back to you as soon as
+            possible!
+          </h4>
+          <form>
+            <div className="wrap">
+              <b>
+                <span className="label">Your Name</span>
+              </b>
+              <input
+                className="input"
+                type="text"
+                name="name"
+                placeholder="Enter your name"
+                required
+              />
+              <span className="focus-input"></span>
+            </div>
+
+            <div className="wrap">
+              <span className="label">Email</span>
+              <input
+                className="input"
+                type="email"
+                name="email"
+                placeholder="Enter your email addess"
+                required
+              />
+              <span className="focus-input"></span>
+            </div>
+
+            <div className="wrap">
+              <span className="label">Message</span>
+              <textarea
+                className="input"
+                name="message"
+                placeholder="Your message here..."
+                required
+              ></textarea>
+              <span className="focus-input"></span>
+            </div>
+
+            <div className="submit">
+              <button className="msg-btn" type="submit">
+                {" "}
+                Send Message{" "}
+              </button>
+            </div>
+          </form>
+        </section>
       </React.Fragment>
     );
   }
